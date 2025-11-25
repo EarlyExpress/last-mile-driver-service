08e861af391dabe2bcf6a76b512c624e9e170cc5
jun <pickjog@gmail.com>
2025-11-25 18:53:57 +0900

    chore(build): 전체 환경설정

---
+++ src/main/java/com/early_express/last_mile_driver_service/LastMileDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.early_express.last_mile_driver_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastMileDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastMileDeliveryServiceApplication.class, args);
+	}
+
+}
